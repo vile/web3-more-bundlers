c0a191e16b99ba6e31723b47e8ffe49c439ee133
Vile <111662603+vile@users.noreply.github.com>
2024-03-18 05:32:16 -0400

    fix: explicitly init base flashbot middleware

--- web3morebundlers/__init__.py
+++ web3morebundlers/__init__.py
@@ -1,5 +1,5 @@
 from eth_account.signers.local import LocalAccount
-from flashbots import Flashbots
+from flashbots import Flashbots, flashbot
 from web3 import Web3
 from web3._utils.module import attach_modules
 
@@ -8,6 +8,7 @@ from .bundlers_provider import BundlersProvider
 
 
 def bundler(w3: Web3, signature_account: LocalAccount, endpoint_uris: list) -> None:
+    flashbot(w3, signature_account, "https://relay.flashbots.net")
     providers = BundlersProvider(signature_account, endpoint_uris)
     bundlers_middleware = construct_bundlers_middleware(providers)
     w3.middleware_onion.add(bundlers_middleware)
