c785f497284377bef4335ba30631e00e431e745e
Vile <111662603+vile@users.noreply.github.com>
2024-03-18 22:37:01 -0400

    refactor: improve typing, doc string
    - sort imports
    - explicitly type `endpoint_uris`
    - add doc string

--- web3morebundlers/__init__.py
+++ web3morebundlers/__init__.py
@@ -1,18 +1,23 @@
+from typing import Any, Optional
+
 from eth_account.signers.local import LocalAccount
 from flashbots import flashbot
 from web3 import Web3
 
 from .bundlers_middleware import construct_bundlers_middleware
 from .bundlers_provider import BundlersProvider
-from typing import Optional, Any
 
 
 def bundler(
     w3: Web3,
     signature_account: LocalAccount,
-    endpoint_uris: list,
+    endpoint_uris: list[str],
     flashbots_uri: Optional[Any] = None,
 ) -> None:
+    """
+    Injects the base flashbots module and middleware, as well as bundler middleware to w3.
+    """
+
     _flashbots_uri = flashbots_uri or "https://relay.flashbots.net"
     flashbot(w3, signature_account, _flashbots_uri)
     providers = BundlersProvider(signature_account, endpoint_uris)
